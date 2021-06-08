b1a5b2314215f1c670871e962e3eb808d55785f2
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-06-08 21:28:09 +0800

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+UnitTypes.alpha.weapons.each(s => s.shots = 20);
+UnitTypes.alpha.weapons.each(i => i.inaccuracy = 10);
+UnitTypes.alpha.weapons.get(0).bullet.speed = 6;
+UnitTypes.alpha.weapons.get(0).bullet.damage = 400;
+UnitTypes.alpha.weapons.get(0).bullet.buildingDamageMultiplier = 1;
+UnitTypes.alpha.speed = 7;
+UnitTypes.alpha.buildSpeed = 10;
+UnitTypes.alpha.mineSpeed = 200:
+UnitTypes.beta.weapons.each(s => s.shots = 20);
+UnitTypes.beta.weapons.each(i => i.inaccuracy = 10);
+UnitTypes.beta.weapons.get(0).bullet.speed = 6;
+UnitTypes.beta.weapons.get(0).bullet.damage = 400;
+UnitTypes.beta.weapons.get(0).bullet.buildingDamageMultiplier = 1;
+UnitTypes.beta.speed = 7;
+UnitTypes.beta.buildSpeed = 10;
+UnitTypes.beta.mineSpeed = 200:
+UnitTypes.gamma.weapons.each(s => s.shots = 20);
+UnitTypes.gamma.weapons.each(i => i.inaccuracy = 10);
+UnitTypes.gamma.weapons.get(0).bullet.speed = 6;
+UnitTypes.gamma.weapons.get(0).bullet.damage = 400;
+UnitTypes.gamma.weapons.get(0).bullet.buildingDamageMultiplier = 1;
+UnitTypes.gamma.speed = 7;
+UnitTypes.gamma.buildSpeed = 10;
+UnitTypes.gamma.mineSpeed = 200:
