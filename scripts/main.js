4d5d70017916e3efc8b757611e84f099d1395f21
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-06-12 17:58:33 +0800

    not nerf

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-UnitTypes.alpha.weapons.each(s => s.shots = 20)
+UnitTypes.alpha.weapons.each(s => s.shots = 50)
 UnitTypes.alpha.weapons.each(i => i.inaccuracy = 10)
 UnitTypes.alpha.weapons.get(0).bullet.speed = 6
 UnitTypes.alpha.weapons.get(0).bullet.damage = 200
@@ -6,9 +6,9 @@ UnitTypes.alpha.weapons.get(0).bullet.buildingDamageMultiplier = 1
 UnitTypes.alpha.speed = 7
 UnitTypes.alpha.buildSpeed = 10
 UnitTypes.alpha.mineSpeed = 200
-UnitTypes.alpha.health = 1000
+UnitTypes.alpha.health = 1500
 UnitTypes.alpha.armor = 1000
-UnitTypes.beta.weapons.each(s => s.shots = 20)
+UnitTypes.beta.weapons.each(s => s.shots = 50)
 UnitTypes.beta.weapons.each(i => i.inaccuracy = 10)
 UnitTypes.beta.weapons.get(0).bullet.speed = 6
 UnitTypes.beta.weapons.get(0).bullet.damage = 200
@@ -16,9 +16,9 @@ UnitTypes.beta.weapons.get(0).bullet.buildingDamageMultiplier = 1
 UnitTypes.beta.speed = 7
 UnitTypes.beta.buildSpeed = 10
 UnitTypes.beta.mineSpeed = 200
-UnitTypes.beta.health = 1000
+UnitTypes.beta.health = 1500
 UnitTypes.beta.armor = 1000
-UnitTypes.gamma.weapons.each(s => s.shots = 20)
+UnitTypes.gamma.weapons.each(s => s.shots = 50)
 UnitTypes.gamma.weapons.each(i => i.inaccuracy = 10)
 UnitTypes.gamma.weapons.get(0).bullet.speed = 6
 UnitTypes.gamma.weapons.get(0).bullet.damage = 200
@@ -26,5 +26,5 @@ UnitTypes.gamma.weapons.get(0).bullet.buildingDamageMultiplier = 1
 UnitTypes.gamma.speed = 7
 UnitTypes.gamma.buildSpeed = 10
 UnitTypes.gamma.mineSpeed = 200
-UnitTypes.gamma.health = 1000
+UnitTypes.gamma.health = 1500
 UnitTypes.gamma.armor = 1000
