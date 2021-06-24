8776cb95f6f610bb7cfbabda1265796263711d77
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-06-24 11:45:44 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ UnitTypes.alpha.weapons.get(0).bullet.buildingDamageMultiplier = 1
 UnitTypes.alpha.speed = 7
 UnitTypes.alpha.buildSpeed = 10
 UnitTypes.alpha.mineSpeed = 200
+UnitTypes.alpha.mineTier = 200
 UnitTypes.alpha.health = 1500
 UnitTypes.alpha.armor = 1000
 UnitTypes.beta.weapons.each(s => s.shots = 50)
@@ -16,6 +17,7 @@ UnitTypes.beta.weapons.get(0).bullet.buildingDamageMultiplier = 1
 UnitTypes.beta.speed = 7
 UnitTypes.beta.buildSpeed = 10
 UnitTypes.beta.mineSpeed = 200
+UnitTypes.beta.mineTier = 200
 UnitTypes.beta.health = 1500
 UnitTypes.beta.armor = 1000
 UnitTypes.gamma.weapons.each(s => s.shots = 50)
@@ -26,5 +28,6 @@ UnitTypes.gamma.weapons.get(0).bullet.buildingDamageMultiplier = 1
 UnitTypes.gamma.speed = 7
 UnitTypes.gamma.buildSpeed = 10
 UnitTypes.gamma.mineSpeed = 200
+UnitTypes.gamma.mineTier = 200
 UnitTypes.gamma.health = 1500
 UnitTypes.gamma.armor = 1000
